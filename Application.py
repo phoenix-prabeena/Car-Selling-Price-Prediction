4001e0d94fafac4bf6a1baa5715f3081942b39e7
phoenix-prabeena <prabeena.pradhan@hotmail.com>
2024-07-10 15:30:57 +0530

    Update Application.py

--- Application.py
+++ Application.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 import numpy as np
 
-with open('Linearregression.pkl', 'rb') as file:
+with open('Lineararegression.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Title of the app
